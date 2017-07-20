31bbbe4027b14739bdbe9d529adbc870e35e08d1
irtimaled <irtimaled@gmail.com>
2017-07-20 10:50:38 -0700

    Support forge clients without the mod

--- java/com/irtimaled/bbor/forge/ForgeMod.java
+++ java/com/irtimaled/bbor/forge/ForgeMod.java
@@ -9,13 +9,13 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
-@Mod(modid = ForgeMod.MODID, name = ForgeMod.NAME, version = ForgeMod.VERSION, acceptedMinecraftVersions = ForgeMod.MCVERSION)
+@Mod(modid = ForgeMod.MODID, name = ForgeMod.NAME, version = ForgeMod.VERSION, acceptedMinecraftVersions = ForgeMod.MCVERSION, acceptableRemoteVersions = "*")
 public class ForgeMod {
 
     public static final String MODID = "bbor";
     public static final String NAME = "Bounding Box Outline Reloaded";
-    public static final String VERSION = "1.0.0-beta15";
-    public static final String MCVERSION = "1.11.2";
+    public static final String VERSION = "1.0.0-beta18";
+    public static final String MCVERSION = "1.12";
 
     private ConfigManager configManager;
 
