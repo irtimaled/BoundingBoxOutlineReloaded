bc5da28a7575f8a7d6613bf1a8fbf4938e880fff
irtimaled <irtimaled@gmail.com>
2019-01-30 17:36:36 -0800

    Update tweaker for 1.13.1

--- src/main/java/com/irtimaled/bbor/mixin/server/MixinMinecraftServer.java
+++ src/main/java/com/irtimaled/bbor/mixin/server/MixinMinecraftServer.java
@@ -4,20 +4,24 @@ import com.irtimaled.bbor.client.BoundingBoxOutlineReloaded;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Map;
+
 @Mixin(MinecraftServer.class)
 public class MixinMinecraftServer {
-    @Shadow public WorldServer[] worlds;
+    @Shadow @Final private Map<DimensionType, WorldServer> worlds;
 
     @Inject(method = "initialWorldChunkLoad", at = @At("HEAD"))
     private void initialWorldChunkLoad(CallbackInfo ci)
     {
-        for(World world : worlds) {
+        for(World world : worlds.values()) {
             BoundingBoxOutlineReloaded.worldLoaded(world);
         }
     }
