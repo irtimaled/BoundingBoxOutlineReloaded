c58984aa6cc14f34b322f203b35726b7f68a27bd
Irtimaled <irtimaled@gmail.com>
2019-03-14 22:42:02 -0700

    Handle unexpected disconnects

--- src/main/java/com/irtimaled/bbor/mixin/client/network/MixinNetHandlerLoginClient.java
+++ src/main/java/com/irtimaled/bbor/mixin/client/network/MixinNetHandlerLoginClient.java
@@ -1,10 +1,12 @@
 package com.irtimaled.bbor.mixin.client.network;
 
 import com.irtimaled.bbor.client.events.ConnectedToRemoteServer;
+import com.irtimaled.bbor.client.events.DisconnectedFromRemoteServer;
 import com.irtimaled.bbor.common.EventBus;
 import net.minecraft.client.network.NetHandlerLoginClient;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.login.server.SPacketLoginSuccess;
+import net.minecraft.util.text.ITextComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,7 +21,12 @@ public abstract class MixinNetHandlerLoginClient {
     private NetworkManager networkManager;
 
     @Inject(method = "handleLoginSuccess", at = @At(value = "RETURN"))
-    private void handleLoginSuccess(SPacketLoginSuccess packet, CallbackInfo ci) {
+    private void handleLoginSuccess(CallbackInfo ci) {
         EventBus.publish(new ConnectedToRemoteServer(this.networkManager));
+    }
+
+    @Inject(method = "onDisconnect", at=@At("HEAD"))
+    private void onDisconnect(CallbackInfo ci) {
+        EventBus.publish(new DisconnectedFromRemoteServer());
     }
 }
---
+++ src/main/java/com/irtimaled/bbor/mixin/client/network/MixinNetHandlerPlayClient.java
@@ -0,0 +1,18 @@
+package com.irtimaled.bbor.mixin.client.network;
+
+import com.irtimaled.bbor.client.events.DisconnectedFromRemoteServer;
+import com.irtimaled.bbor.common.EventBus;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.util.text.ITextComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetHandlerPlayClient.class)
+public class MixinNetHandlerPlayClient {
+    @Inject(method = "onDisconnect", at=@At("HEAD"))
+    private void onDisconnect(CallbackInfo ci) {
+        EventBus.publish(new DisconnectedFromRemoteServer());
+    }
+}
