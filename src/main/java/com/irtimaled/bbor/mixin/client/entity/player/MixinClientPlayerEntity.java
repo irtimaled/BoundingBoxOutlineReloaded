7e52890aa14bb2a6904457b51cb5db3ac1cdd65e
violetc <58360096+s-yh-china@users.noreply.github.com>
2022-08-04 18:45:48 +0800

    update to 1.19.1

--- src/main/java/com/irtimaled/bbor/mixin/client/entity/player/MixinClientPlayerEntity.java
+++ src/main/java/com/irtimaled/bbor/mixin/client/entity/player/MixinClientPlayerEntity.java
@@ -2,6 +2,7 @@ package com.irtimaled.bbor.mixin.client.entity.player;
 
 import com.irtimaled.bbor.client.interop.ClientInterop;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayerEntity.class)
 public class MixinClientPlayerEntity {
     @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
-    private void sendChatMessage(String message, CallbackInfo ci) {
+    private void sendChatMessage(String message, Text preview, CallbackInfo ci) {
         if (ClientInterop.interceptChatMessage(message))
             ci.cancel();
     }
