fb5b59298f78979343a04af9c51fa136a4c8297e
ishland <ishlandmc@yeah.net>
2022-08-09 11:26:50 +0800

    Fixes beacon

--- src/main/java/com/irtimaled/bbor/client/providers/BeaconProvider.java
+++ src/main/java/com/irtimaled/bbor/client/providers/BeaconProvider.java
@@ -14,30 +14,21 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectMaps;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import it.unimi.dsi.fastutil.objects.ObjectList;
 import net.minecraft.block.entity.BeaconBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 
 public class BeaconProvider implements IBoundingBoxProvider<BoundingBoxBeacon> {
 
-    private final Long2ObjectMap<BoundingBoxBeacon> boxes = Long2ObjectMaps.synchronize(new Long2ObjectLinkedOpenHashMap<>(), this);
-    private ObjectList<BoundingBoxBeacon> boxesCopy = new ObjectArrayList<>(this.boxes.values());
+    private static final Long2ObjectMap<BoundingBoxBeacon> boxes = Long2ObjectMaps.synchronize(new Long2ObjectLinkedOpenHashMap<>(), BeaconProvider.class);
+    private static ObjectList<BoundingBoxBeacon> boxesCopy = new ObjectArrayList<>(boxes.values());
 
-    {
+    static {
         EventBus.subscribe(ClientWorldUpdateTracker.BlockEntityAddEvent.class, event -> {
-            if (event.blockEntity() instanceof BeaconBlockEntity beacon) {
-                int levels = ((IBeaconBlockEntity) beacon).getLevel1();
-                Coords coords = new Coords(beacon.getPos());
-                boxes.put(new ChunkPos(beacon.getPos()).toLong(), BoundingBoxBeacon.from(coords, levels));
-                updateCopy();
-            }
+            updateOrCreateBeacon(event.blockEntity());
         });
         EventBus.subscribe(ClientWorldUpdateTracker.BlockEntityUpdateEvent.class, event -> {
-            if (event.blockEntity() instanceof BeaconBlockEntity beacon) {
-                int levels = ((IBeaconBlockEntity) beacon).getLevel1();
-                Coords coords = new Coords(beacon.getPos());
-                boxes.put(new ChunkPos(beacon.getPos()).toLong(), BoundingBoxBeacon.from(coords, levels));
-                updateCopy();
-            }
+            updateOrCreateBeacon(event.blockEntity());
         });
         EventBus.subscribe(ClientWorldUpdateTracker.BlockEntityRemoveEvent.class, event -> {
             boxes.remove(new ChunkPos(new BlockPos(event.x(), event.y(), event.z())).toLong());
@@ -45,9 +36,20 @@ public class BeaconProvider implements IBoundingBoxProvider<BoundingBoxBeacon> {
         });
     }
 
-    private void updateCopy() {
-        synchronized (this) {
-            this.boxesCopy = new ObjectArrayList<>(this.boxes.values());
+    public static void updateOrCreateBeacon(BlockEntity blockEntity) {
+        if (blockEntity instanceof BeaconBlockEntity beacon) {
+            int levels = ((IBeaconBlockEntity) beacon).getLevel1();
+            final long key = ChunkPos.toLong(blockEntity.getPos());
+            if (boxes.containsKey(key) && boxes.get(key).getLevel() == levels) return;
+            Coords coords = new Coords(beacon.getPos());
+            boxes.put(key, BoundingBoxBeacon.from(coords, levels));
+            updateCopy();
+        }
+    }
+
+    private static void updateCopy() {
+        synchronized (BeaconProvider.class) {
+            boxesCopy = new ObjectArrayList<>(boxes.values());
         }
     }
 
