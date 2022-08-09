a704e2c1ede7445fcf5332a038207f3f8b4c75ff
ishland <ishlandmc@yeah.net>
2022-08-09 12:11:04 +0800

    custom providers

--- src/main/java/com/irtimaled/bbor/client/providers/CustomLineProvider.java
+++ src/main/java/com/irtimaled/bbor/client/providers/CustomLineProvider.java
@@ -6,12 +6,11 @@ import com.irtimaled.bbor.client.models.Point;
 import com.irtimaled.bbor.common.BoundingBoxType;
 import com.irtimaled.bbor.common.models.DimensionId;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CustomLineProvider implements IBoundingBoxProvider<BoundingBoxLine>, ICachingProvider {
-    private static final Map<DimensionId, Map<Integer, BoundingBoxLine>> dimensionCache = new HashMap<>();
+    private static final Map<DimensionId, Map<Integer, BoundingBoxLine>> dimensionCache = new ConcurrentHashMap<>();
 
     private static int getHashKey(Point minPoint, Point maxPoint) {
         return (31 + minPoint.hashCode()) * 31 + maxPoint.hashCode();
