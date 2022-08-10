9cd9868c7d00d49e07868ca099121014a382fc8c
ishland <ishlandmc@yeah.net>
2022-08-10 23:13:15 +0800

    Remove unused stuff

--- src/main/java/com/irtimaled/bbor/client/renderers/RenderHelper.java
+++ src/main/java/com/irtimaled/bbor/client/renderers/RenderHelper.java
@@ -3,8 +3,6 @@ package com.irtimaled.bbor.client.renderers;
 import com.irtimaled.bbor.client.config.ConfigManager;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Quaternion;
 import org.lwjgl.opengl.GL11;
 
 public class RenderHelper {
@@ -32,35 +30,6 @@ public class RenderHelper {
         RenderSystem.setShaderColor(1, 1, 1, 1);
     }
 
-    public static void beforeRenderFont(MatrixStack matrixStack, OffsetPoint offsetPoint) {
-        RenderSystem.assertOnRenderThread();
-        matrixStack.push();
-        polygonModeFill();
-        matrixStack.translate(offsetPoint.getX(), offsetPoint.getY() + 0.002D, offsetPoint.getZ());
-        // GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-        matrixStack.multiply(new Quaternion(0.0F, 0.0F, 0.0F, 1.0F));
-        matrixStack.multiply(new Quaternion(0.0F, 90.0F, 1.0F, 0.0F));
-        matrixStack.scale(-0.0175F, -0.0175F, 0.0175F);
-        enableTexture();
-        enableBlend();
-        GlStateManager._blendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
-
-        depthMaskTrue();
-    }
-
-    public static void afterRenderFont(MatrixStack matrixStack) {
-        disableTexture();
-        disableBlend();
-        RenderSystem.assertOnRenderThread();
-        matrixStack.pop();
-        enableDepthTest();
-    }
-
-//    public static void disableLighting() {
-//        RenderSystem.assertThread(RenderSystem::isOnRenderThread);
-//        GL11.glDisable(GL11.GL_LIGHTING);
-//    }
-
     public static void disableDepthTest() {
         GlStateManager._disableDepthTest();
     }
@@ -68,11 +37,6 @@ public class RenderHelper {
     public static void enableDepthTest() {
         GlStateManager._enableDepthTest();
     }
-
-//    public static void disableFog() {
-//        RenderSystem.assertThread(RenderSystem::isOnRenderThread);
-//        GL11.glDisable(GL11.GL_FOG);
-//    }
 
     public static void disableBlend() {
         GlStateManager._disableBlend();
@@ -82,62 +46,12 @@ public class RenderHelper {
         GlStateManager._enableBlend();
     }
 
-//    public static void disableAlphaTest() {
-//        RenderSystem.assertThread(RenderSystem::isOnRenderThread);
-//        GL11.glDisable(GL32.GL_ALPHA_TEST);
-//    }
-//
-//    public static void enableAlphaTest() {
-//        RenderSystem.assertThread(RenderSystem::isOnRenderThread);
-//        GL11.glEnable(GL11.GL_ALPHA_TEST);
-//    }
-
     public static void disableTexture() {
         GlStateManager._disableTexture();
     }
 
     public static void enableTexture() {
         GlStateManager._enableTexture();
-    }
-
-//    public static void shadeModelSmooth() {
-//        RenderSystem.assertThread(RenderSystem::isOnRenderThreadOrInit);
-//        GL11.glShadeModel(GL11.GL_SMOOTH);
-//    }
-//
-//    public static void shadeModelFlat() {
-//        RenderSystem.assertThread(RenderSystem::isOnRenderThreadOrInit);
-//        GL11.glShadeModel(GL11.GL_FLAT);
-//    }
-
-//    public static void enablePointSmooth() {
-//        GL11.glEnable(GL11.GL_POINT_SMOOTH);
-//    }
-
-    public static void lineWidth2() {
-        RenderSystem.assertOnRenderThread();
-        RenderSystem.lineWidth(2f);
-    }
-
-    public static void polygonModeLine() {
-        GlStateManager._polygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
-    }
-
-    public static void polygonModeFill() {
-        GlStateManager._polygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
-    }
-
-    public static void polygonOffsetMinusOne() {
-        GlStateManager._polygonOffset(-1.f, -1.f);
-    }
-
-    public static void enablePolygonOffsetLine() {
-        RenderSystem.assertOnRenderThread();
-        GL11.glEnable(GL11.GL_POLYGON_OFFSET_LINE);
-    }
-
-    public static void depthMaskTrue() {
-        GlStateManager._depthMask(true);
     }
 
     public static void blendFuncGui() {
