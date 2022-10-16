4e80877de6f5d48bc7d9336fa3d356d87e745ec5
s-yh-china <58360096+s-yh-china@users.noreply.github.com>
2022-10-16 14:19:13 +0800

    Fix bug that the forge version cannot run on the server (#169)

--- src/main/java/com/irtimaled/bbor/BBORForgeMod.java
+++ src/main/java/com/irtimaled/bbor/BBORForgeMod.java
@@ -1,7 +1,23 @@
 package com.irtimaled.bbor;
 
+import com.irtimaled.bbor.common.CommonProxy;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("bbor")
 public class BBORForgeMod {
+
+    public BBORForgeMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().register(new BBORForgeModListener());
+    }
+
+    public static class BBORForgeModListener {
+        @SubscribeEvent
+        public void onDedicatedSetup(FMLDedicatedServerSetupEvent event) {
+            CommonProxy.isServer = true;
+            new CommonProxy().init();
+        }
+    }
 }
