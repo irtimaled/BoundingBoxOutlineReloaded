8d9a88216b9f4177c17583bba295287dfce16efb
ishland <ishlandmc@yeah.net>
2023-01-09 21:35:02 +0800

    Fixes forge

--- src/main/java/com/irtimaled/bbor/BBORForgeMod.java
+++ src/main/java/com/irtimaled/bbor/BBORForgeMod.java
@@ -1,23 +1,31 @@
 package com.irtimaled.bbor;
 
 import com.irtimaled.bbor.common.CommonProxy;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Mod("bbor")
 public class BBORForgeMod {
 
+    private static final AtomicBoolean initialized = new AtomicBoolean(false);
+
     public BBORForgeMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().register(new BBORForgeModListener());
+        MinecraftForge.EVENT_BUS.register(new BBORForgeModListener());
     }
 
     public static class BBORForgeModListener {
         @SubscribeEvent
-        public void onDedicatedSetup(FMLDedicatedServerSetupEvent event) {
-            CommonProxy.isServer = true;
-            new CommonProxy().init();
+        public void onSetup(ServerAboutToStartEvent event) {
+            if (FMLEnvironment.dist == Dist.DEDICATED_SERVER && initialized.compareAndSet(false, true)) {
+                CommonProxy.isServer = true;
+                new CommonProxy().init();
+            }
         }
     }
 }
