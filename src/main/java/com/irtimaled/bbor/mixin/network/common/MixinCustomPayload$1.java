134efbb28b2646ef655de017c7ba65acdb7fff55
ishland <ishlandmc@yeah.net>
2024-11-23 15:24:58 +0800

    fix: serverside issue

--- src/main/java/com/irtimaled/bbor/mixin/network/common/MixinCustomPayload$1.java
+++ src/main/java/com/irtimaled/bbor/mixin/network/common/MixinCustomPayload$1.java
@@ -1,7 +1,6 @@
 package com.irtimaled.bbor.mixin.network.common;
 
 import com.irtimaled.bbor.common.BBORCustomPayload;
-import com.irtimaled.bbor.common.messages.servux.ServuxStructurePackets;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.CustomPayload;
@@ -16,13 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(targets = "net.minecraft.network.packet.CustomPayload$1")
 public class MixinCustomPayload$1 {
 
+    private static final Identifier BBOR_SERVUX_CHANNEL = Identifier.tryParse("servux:structures");
+
     @Inject(
             method = "decode(Lnet/minecraft/network/PacketByteBuf;)Lnet/minecraft/network/packet/CustomPayload;",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/CustomPayload$1;getCodec(Lnet/minecraft/util/Identifier;)Lnet/minecraft/network/codec/PacketCodec;"),
             cancellable = true
     )
     private <B extends PacketByteBuf> void onCustomPayloadDecode(B packetByteBuf, CallbackInfoReturnable<CustomPayload> cir, @Local @NotNull Identifier identifier) {
-        if (identifier.getNamespace().equals("bbor") || identifier.equals(ServuxStructurePackets.CHANNEL)) {
+        if (identifier.getNamespace().equals("bbor") || identifier.equals(BBOR_SERVUX_CHANNEL)) {
             cir.setReturnValue(new BBORCustomPayload(identifier, packetByteBuf));
         }
     }
--- src/main/java/com/irtimaled/bbor/mixin/server/network/MixinServerPlayNetworkHandler.java
+++ src/main/java/com/irtimaled/bbor/mixin/server/network/MixinServerPlayNetworkHandler.java
@@ -2,18 +2,16 @@ package com.irtimaled.bbor.mixin.server.network;
 
 import com.irtimaled.bbor.common.BBORCustomPayload;
 import com.irtimaled.bbor.common.interop.CommonInterop;
-import com.irtimaled.bbor.common.messages.PayloadReader;
 import com.irtimaled.bbor.common.messages.SubscribeToServer;
 import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
-import net.minecraft.server.network.ServerCommonNetworkHandler;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerCommonNetworkHandler.class)
-public class MixinServerCommonNetworkHandler {
+@Mixin(ServerPlayNetworkHandler.class)
+public class MixinServerPlayNetworkHandler {
 
     @Inject(method = "onCustomPayload", at = @At(value = "HEAD"), cancellable = true)
     private void onCustomPayload(CustomPayloadC2SPacket packet, CallbackInfo ci) {
